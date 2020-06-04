64e7f707e8689d1d3b19b42f01a0fc101c953825
unknown <israelcarneiro97@gmail.com>
2020-06-04 05:36:44 -0300

    Server online e primeira API REST testada

---
+++ main.ts
@@ -0,0 +1,14 @@
+import * as restify from 'restify';
+import {Server} from './server/server'
+import {usersRouter} from './users/users.router'
+
+const server = new Server()
+server.bootstrap([usersRouter]).then(server=>{
+	console.log('api is running successfully on: ', server.application.address())
+}).catch(error => {
+	console.log('Server failed to start')
+	console.error(error)
+	process.exit(1)
+})
+
+
